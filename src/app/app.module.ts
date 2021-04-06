5490e3d1ce0f48c64063e40013127fc519226392
Vendula Buchtova <ven.buchtova@gmail.com>
2021-04-06 23:21:31 +0200

    day 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { TechnoComponent } from './techno/techno.component';
+import { WorkComponent } from './work/work.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    AboutComponent,
+    TechnoComponent,
+    WorkComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/techno/techno.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-techno',
+  templateUrl: './techno.component.html',
+  styleUrls: ['./techno.component.scss']
+})
+export class TechnoComponent implements OnInit {
+Techno = [
+  {
+      name: "HTML",
+      img: "../../assets/img/html.png",
+      width: 90
+    },{
+      name: "CSS",
+      img: "../../assets/img/css.png",
+      width: 90
+    },{
+      name: "JavaScript",
+      img: "../../assets/img/js.png",
+      width: 50
+    },{
+      name: "GitHub",
+      img: "../../assets/img/github.png",
+      width: 95
+    },{
+      name: "Angular",
+      img: "../../assets/img/angular.png",
+      width: 80
+    },{
+      name: "Sass",
+      img: "../../assets/img/sass.png",
+      width: 80
+    },{
+      name: "TypeScript",
+      img: "../../assets/img/type.png",
+      width: 70
+    },{
+      name: "Bootstrap",
+      img: "../../assets/img/html.png",
+      width: 90
+    }
+]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
